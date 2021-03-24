188e0735bc23f9ea99ee7922237dd0e5450c076c
itproto <itprotoltd@gmail..com>
2021-03-24 21:59:20 +0200

    f

--- iaac/main.tf
+++ iaac/main.tf
@@ -119,18 +119,7 @@ resource "aws_instance" "web" {
 
   // https://gist.github.com/wdullaer/f1af16bd7e970389bad3
   provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y update",
-      "sudo yum install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo docker run hello-world",
-      "sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > docker-compose",
-      "sudo mv docker-compose /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "sudo chmod +x /app/sh/reboot.sh",
-      "sudo /usr/local/bin/docker-compose up --build -d" //--scale web=2"
-    ]
+    script = "./sh/boot.sh"
     connection {
       type = "ssh"
       user = "ec2-user"
