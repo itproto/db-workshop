926289b307cdba9dcdfca8020d3c9cad90a2c942
itproto <itprotoltd@gmail..com>
2021-03-26 10:32:36 +0200

    f

--- jupyter/docker-entrypoint.sh
+++ jupyter/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-start-notebook.sh --NotebookApp.port=9999 --NotebookApp.notebook_dir=/srv/notebooks/
-# jupyter kernelgateway --KernelGatewayApp.port=7777 --KernelGatewayApp.api=notebook-http --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.seed_uri=/srv/notebooks/hello.ipynb
+start-notebook.sh --NotebookApp.port=9999 --NotebookApp.notebook_dir=/srv/notebooks/ &
+jupyter kernelgateway --KernelGatewayApp.port=7777 --KernelGatewayApp.api=notebook-http --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.seed_uri=/srv/notebooks/hello.ipynb
 echo "Hello World"
 exec "$@"
--- sh/mount.sh
+++ sh/mount.sh
@@ -1,1 +1,1 @@
-docker run -v //c/ws/db-workshop/jupyter/book/:/home/jovyan/work:Z --user root -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' -p 8888:8888 hello-jup
+docker run -v //c/ws/db-workshop/jupyter/book/:/srv/notebooks:Z --user root -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' -p 7777:7777 -p 9999:9999 hello-jup
