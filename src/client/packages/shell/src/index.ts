90cfe914bcc44f8c2567e3f8fac0cbeba512c837
itproto <itprotoltd@gmail..com>
2021-03-26 22:48:57 +0200

    Moved lerna

--- src/client/packages/shell/src/index.ts
+++ src/client/packages/shell/src/index.ts
@@ -1,14 +1,5 @@
-// import template from 'lodash.template';
 
 const $el = document.querySelector('#root');
 if ($el) {
-  $el.innerHTML = `Hello world`
-  /*var compiled = template(`
-    <h1><%- heading %></h1>
-    Current date and time: <%- dateTimeString %>
-  `);
-  $el.innerHTML = compiled({
-    heading: 'ts-demo-webpack1',
-    dateTimeString: new Date().toISOString(),
-  });*/
+  $el.innerHTML = `Shell`
 }
